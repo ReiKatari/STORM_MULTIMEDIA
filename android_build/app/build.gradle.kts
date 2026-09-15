31ced53f159e2ae4a07ad20defecfdd6127c0d95
ReiKatari <45316432+ReiKatari@users.noreply.github.com>
2026-09-16 02:57:14 +0300

    feat: исправление системных отступов, масштабирования WebView, переходов навигации и обновление APK до 0.0.9

--- android_build/app/build.gradle.kts
+++ android_build/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.reikatari.stormmultimedia"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
     }
 
     signingConfigs {
